d5a41b3aa1d29a4d01acd385cdd946ba944531fc
VaniLSatc <iwan.ivan432@yandex.ru>
2025-10-22 19:37:59 +0700

    Update mainProgram.cpp

--- mainProgram.cpp
+++ mainProgram.cpp
@@ -1,129 +1,162 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <filesystem>
-#include <locale>
+#include <cstdlib>
+#include "Platform.h"
 
 using namespace std;
 
-void Caesar_Cipher(int console_answer) {
-	if (console_answer == 1) { //Шифр цезаря. Процесс шифрования исходного текста из файла
-		string inputFile;
-		cout << "Введите название файла, который необходимо зашифровать: ";
-		cin >> inputFile; 
+typedef void (*CaesarCipher)(int);
+typedef void (*XORCipher)(int);
+typedef void (*DTPCipher)(int);
 
-		ifstream fin(inputFile, ios::binary);
-		ofstream fout("EncruptedFile.bin", ios::binary);
+namespace ConsoleUtils {
+    void clearConsole() {
+    #ifdef _WIN32
+        system("cls");
+    #else
+        system("clear");
+    #endif
+    }
 
-		if (!fin.is_open() || !fout.is_open()) {
-			cerr << "Ошибка открытия файла для чтения" << endl;
-			exit(0);
-		}
-
-		cout << "Придумайте пароль. Его нужно будет указать при расшифровке для получения доступа к данным. Не потеряйте его!\n";
-		string password_;
-		cin >> password_;
-		cout << "Также для шифрования вам понадобится ключ.\nЕго значение вы должны запомнить! Введите значение ключа: ";
-		int caesarKey;
-		cin >> caesarKey;
-
-		for (char i : password_) {
-			if (i > 255) fout.put((i + caesarKey) % 256);
-			else fout.put(char(i + caesarKey));
-		}
-
-		fout << " "; // Неккоректно дешифруется, нужно считывать пароль байтами
-
-		char text;
-		while (fin.get(text)) {
-			if (text > 255) fout.put((text + caesarKey) % 256);
-			else fout.put(char(text + caesarKey));
-		}
-
-		fin.close();
-		fout.close();
-
-	}
-	if (console_answer == 2) {//Шифр цезаря. Процесс дешифрования исходного текста из файла
-		cout << "Вы пытаетесь прочитать текст, защищённый шифром Цезаря. Введите ключ: ";
-		int caesarKey;
-		cin >> caesarKey;
-
-		string inputFile;
-		cout << "Введите название файла, который необходимо расшифровать: ";
-		cin >> inputFile;
-
-		//Расшифровываем пароль с указанным ключем
-		ifstream fin(inputFile, ios::binary);
-		string EncryptPassword_, DecryptPassword_; //Создаём переменные для приёма пароля из файла и для хранения расшифрованного пароля
-		fin >> EncryptPassword_; //Первое слово в зашифрованном тексте(если его никто не менял) - зашифрованный пароль
-		for (char i : EncryptPassword_) {
-			DecryptPassword_ += char(i - caesarKey);
-		}
-
-		cout << "Введите пароль: ";
-		string userTryPassword_;
-		cin >> userTryPassword_;
-
-		for (int i = 0; i < 3; i++) {
-			if (userTryPassword_ == DecryptPassword_) { cout << "Пароль верный!\n"; break; }
-			if (i == 3) { cout << "Попытки кончились."; exit(0); }
-			cout << "Неверный пароль, попробуйте ещё раз. Попыток осталось: " << 3 - i << endl;
-			cout << "Введите пароль: ";
-			cin >> userTryPassword_;
-		}
-
-		string outputFile;
-		cout << "Введите название расшифрованного файла: ";
-		cin >> outputFile;
-		ofstream fout(outputFile, ios::binary);
-
-		char text;
-		while (fin.get(text)) {
-			if (text > 255) fout.put((text - caesarKey) % 256);
-			else fout.put(char(text - caesarKey));
-		}
-
-		fin.close();
-		fout.close();
-	}
+    void showError(const char* message) {
+    #ifdef _WIN32
+        MessageBoxA(NULL, message, "Error", MB_OK);
+    #else
+        cout << "Ошибка: " << message << endl;
+    #endif
+    }
 }
 
-int main()
-{
-	//Блок 1.Блок для записи исходного текста в файл
-	setlocale(LC_ALL, "Russian");
-	cout << "Здравствуйте! Вы используете программу шифровки/дешифровки текста.\nВведите текст и выберите дальнейшие действия.\nТекст:";
+enum class EncryptionMethod {
+    Caesar,
+    XOR,
+    DoubleTabularPermutation,
+    Exit
+};
 
-	//
+enum class Action {
+    Encrypt,
+    Decrypt
+};
 
-	//Блок 2.Блок для выбора действия. Выбор алгоритма шифрования.
-	cout << "Исходный текст записан. Выберите метод шифрования.\n1) Шифр Цезаря\n2) Шифр XOR\n3) Шифр двойной табличной перестановки\n";
-	int console_answer;
-	cin >> console_answer;
-	while (console_answer != 1 && console_answer != 2 && console_answer != 3) {
-		cout << "Неккоректный выбор, введите ещё раз:\n";
-		cin >> console_answer;
-	}
+void methodChoice(const int input, EncryptionMethod& choice) {
+    switch (input) {
+        case 1: choice = EncryptionMethod::Caesar; break;
+        case 2: choice = EncryptionMethod::XOR; break;
+        case 3: choice = EncryptionMethod::DoubleTabularPermutation; break;
+        case 4: choice = EncryptionMethod::Exit; break;
+        default: break;
+    }
+}
 
-	//
+void actionChoice(const int input, Action& choice) {
+    if (input == 1) choice = Action::Encrypt;
+    else if (input == 2) choice = Action::Decrypt;
+}
 
-	// Блок 3. Запуск функции шифрования/дешифрования.
-	string password;
-	if (console_answer == 1) {
-		cout << "Вы выбрали шифрование текста методом Цезаря.\nВы хотите зашифровать или расшифровать текст?\n1) Зашифровать\n2) Расшифровать\n";
-		cin >> console_answer;
-		while (console_answer != 1 && console_answer != 2) {
-			cout << "Неккоректный выбор, введите ещё раз:\n";
-			cin >> console_answer;
-		}
-		Caesar_Cipher(console_answer);
-		cout << "Успешно! Зашифрованный текст находится в папке проекта в файле 'OutputText.txt'";
-	}
-	if (console_answer == 2) {
-		cout << "Разрабатывается хыхы";
-	}
-	if (console_answer == 3) {
-		cout << "Разрабатывается хыхы";
-	}
+int main() {
+    setlocale(LC_ALL, "Russian");
+    
+    while (true) {
+        ConsoleUtils::clearConsole();
+        cout << "Здравствуйте! Вы используете программу шифровки/дешифровки текста.\n";
+        cout << "Выберите метод шифрования:\n";
+        cout << "1) Шифр Цезаря\n";
+        cout << "2) Шифр XOR\n";
+        cout << "3) Шифр двойной табличной перестановки\n";
+        cout << "4) Выход\n";
+        
+        int console_answer;
+        cin >> console_answer;
+        
+        EncryptionMethod method;
+        methodChoice(console_answer, method);
+        
+        if (method == EncryptionMethod::Exit) break;
+        
+        if (console_answer < 1 || console_answer > 3) {
+            cout << "Некорректный выбор!\n";
+            continue;
+        }
+        
+        cout << "Вы хотите зашифровать или расшифровать данные?\n";
+        cout << "1) Зашифровать\n2) Расшифровать\n";
+        int action_input;
+        cin >> action_input;
+        
+        Action action;
+        actionChoice(action_input, action);
+        
+        if (action_input != 1 && action_input != 2) {
+            cout << "Некорректный выбор!\n";
+            continue;
+        }
+
+        LIB_HANDLE library = nullptr;
+        const char* libName = "";
+        
+        switch (method) {
+        case EncryptionMethod::Caesar:
+        #ifdef _WIN32
+            libName = "CaesarCipherDLL.dll";
+        #else
+            libName = "./libCaesarCipher.so";
+        #endif
+            break;
+        case EncryptionMethod::XOR:
+        #ifdef _WIN32
+            libName = "XORCipherDLL.dll";
+        #else
+            libName = "./libXORCipher.so";
+        #endif
+            break;
+        case EncryptionMethod::DoubleTabularPermutation:
+        #ifdef _WIN32
+            libName = "DoubleTabularPermutation.dll";
+        #else
+            libName = "./libDoubleTabularPermutation.so";
+        #endif
+            break;
+        default:
+            break;
+        }
+        
+        library = LOAD_LIB(libName);
+        if (!library) {
+            ConsoleUtils::showError("Библиотека не найдена!");
+            continue;
+        }
+        
+        // Вызов соответствующей функции
+        int action_code = (action == Action::Encrypt) ? 1 : 2;
+        
+        switch (method) {
+        case EncryptionMethod::Caesar: {
+            CaesarCipher func = (CaesarCipher)GET_PROC(library, "Caesar_Cipher");
+            if (func) func(action_code);
+            break;
+        }
+        case EncryptionMethod::XOR: {
+            XORCipher func = (XORCipher)GET_PROC(library, "XOR_Cipher");
+            if (func) func(action_code);
+            break;
+        }
+        case EncryptionMethod::DoubleTabularPermutation: {
+            DTPCipher func = (DTPCipher)GET_PROC(library, "doubleTablePermutationCipher");
+            if (func) func(action_code);
+            break;
+        }
+        default:
+            break;
+        }
+        
+        if (library) FREE_LIB(library);
+        
+        cout << "\nНажмите Enter для продолжения...";
+        cin.ignore();
+        cin.get();
+    }
+    
+    return 0;
 }
